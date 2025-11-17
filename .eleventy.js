898b238791970a5514820def61edc98a3b224d58
Louie <111729769+NikkeTryHard@users.noreply.github.com>
2025-11-17 03:38:28 -0600

    Use Eleventy default output _site; upload _site for Pages artifact

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: ".",
+      output: "_site",
       includes: "_includes",
       layouts: "_includes"
     },
